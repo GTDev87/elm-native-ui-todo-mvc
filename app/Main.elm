0144539e45b8ba2f773418314e7952f7795d7ab5
Greg <gregt87@gmail.com>
2017-04-19 00:40:55 -0400

    changed indentation

--- app/Main.elm
+++ app/Main.elm
@@ -11,30 +11,30 @@ import NativeUi.Image as Image exposing (..)
 
 
 type alias Model =
-    Int
+  Int
 
 
 model : Model
 model =
-    900123
+  90012
 
 
 -- UPDATE
 
 
 type Msg
-    = Increment
-    | Decrement
+  = Increment
+  | Decrement
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
+  case msg of
+    Increment ->
+      ( model + 1, Cmd.none )
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+    Decrement ->
+      ( model - 1, Cmd.none )
 
 
 
@@ -43,66 +43,66 @@ update msg model =
 
 view : Model -> Node Msg
 view count =
-    let
-        imageSource =
-            { uri = "https://raw.githubusercontent.com/futurice/spiceprogram/master/assets/img/logo/chilicorn_no_text-128.png"
-            , cache = Just ForceCache
-            }
-    in
-        Elements.view
-            [ Ui.style [ Style.alignItems "center" ]
-            ]
-            [ image
-                [ Ui.style
-                    [ Style.height 64
-                    , Style.width 64
-                    , Style.marginBottom 30
-                    , Style.marginTop 30
-                    ]
-                , source imageSource
-                ]
-                []
-            , text
-                [ Ui.style
-                    [ Style.textAlign "center"
-                    , Style.marginBottom 30
-                    ]
-                ]
-                [ Ui.string ("Counter: " ++ toString count)
-                ]
-            , Elements.view
-                [ Ui.style
-                    [ Style.width 80
-                    , Style.flexDirection "row"
-                    , Style.justifyContent "space-between"
-                    ]
-                ]
-                [ button Decrement "#d33" "-"
-                , button Increment "#3d3" "+"
-                ]
-            ]
+  let
+    imageSource =
+      { uri = "https://raw.githubusercontent.com/futurice/spiceprogram/master/assets/img/logo/chilicorn_no_text-128.png"
+      , cache = Just ForceCache
+      }
+  in
+    Elements.view
+      [ Ui.style [ Style.alignItems "center" ]
+      ]
+      [ image
+        [ Ui.style
+          [ Style.height 64
+          , Style.width 64
+          , Style.marginBottom 30
+          , Style.marginTop 30
+          ]
+        , source imageSource
+        ]
+        []
+      , text
+        [ Ui.style
+          [ Style.textAlign "center"
+          , Style.marginBottom 30
+          ]
+        ]
+        [ Ui.string ("Counter: " ++ toString count)
+        ]
+      , Elements.view
+        [ Ui.style
+          [ Style.width 80
+          , Style.flexDirection "row"
+          , Style.justifyContent "space-between"
+          ]
+        ]
+        [ button Decrement "#d33" "-"
+        , button Increment "#3d3" "+"
+        ]
+      ]
 
 
 button : Msg -> String -> String -> Node Msg
 button msg color content =
-    text
-        [ Ui.style
-            [ Style.color "white"
-            , Style.textAlign "center"
-            , Style.backgroundColor color
-            , Style.paddingTop 5
-            , Style.paddingBottom 5
-            , Style.width 30
-            , Style.fontWeight "bold"
-            , Style.shadowColor "#000"
-            , Style.shadowOpacity 0.25
-            , Style.shadowOffset 1 1
-            , Style.shadowRadius 5
-            , Style.transform { defaultTransform | rotate = Just "10deg" }
-            ]
-        , onPress msg
+  text
+      [ Ui.style
+        [ Style.color "white"
+        , Style.textAlign "center"
+        , Style.backgroundColor color
+        , Style.paddingTop 5
+        , Style.paddingBottom 5
+        , Style.width 30
+        , Style.fontWeight "bold"
+        , Style.shadowColor "#000"
+        , Style.shadowOpacity 0.25
+        , Style.shadowOffset 1 1
+        , Style.shadowRadius 5
+        , Style.transform { defaultTransform | rotate = Just "10deg" }
         ]
-        [ Ui.string content ]
+      , onPress msg
+      ]
+      [ Ui.string content ]
 
 
 
@@ -111,9 +111,9 @@ button msg color content =
 
 main : Program Never Model Msg
 main =
-    Ui.program
-        { init = ( model, Cmd.none )
-        , view = view
-        , update = update
-        , subscriptions = \_ -> Sub.none
-        }
+  Ui.program
+    { init = ( model, Cmd.none )
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
